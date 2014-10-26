d0480907bf74b4f80b3413f16e02a1b5b8cf7188
rgargaloyan <rgargaloyan70@yahoo.com>
2014-10-26 14:25:06 -0700

    R Code for the Course Project

---
+++ run_analysis.R
@@ -0,0 +1,43 @@
+############################################
+# Run Analysis.R  does the following: 
+# Merges the training and the test sets to create one data set.
+# Extracts only the measurements on the mean and standard deviation for each measurement. 
+# Uses descriptive activity names to name the activities in the data set
+# Appropriately labels the data set with descriptive variable names. 
+# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+
+#.... First lets create our lookup vectors
+
+#... This will map the activity ID to the activity description
+activityFile <- read.table("activity_labels.txt", header=FALSE, sep=" ")
+activityMap <- as.list(setNames(activityFile$V1, activityFile$V2))
+
+#... Now lets read the features and extact only those column indexes that match the "mean" or "std" pattern
+#    that way, we'll be able to dynamically retrieve the proper column names even of the "feature" file gets 
+#    re-arranged
+
+#... Read the features.txt that maps the column indexes to the data labels
+featureFile <- read.table("features.txt", header=FALSE, sep=" ")
+featureMap <-  as.list(setNames(featureFile$V1, featureFile$V2))
+
+
+#... We'll need to extract only required (mean and std) column indexes from this mess.
+#    Hence, the RegEx search pattern. 
+ptn <- "/(?:mean|std)/g"
+validFeatureColumnIndexes <- subset(featureFile, grepl(ptn, featureFile$V2, perl=T))
+
+
+#...Get test data, eliminate unnecessary columns and combine it with Subject and Activity
+#mergedTestData <- mergeActivitySubjectData("Test","./test/subject_test.txt","./test/y_test.txt","./test/X_test.txt" )
+
+#...Get train data, eliminate unnecessary columns and combine it with Subject and Activity
+#mergedTrainData <- mergeActivitySubjectData("Train","./train/subject_train.txt","./train/y_train.txt","./train/X_train.txt" )
+
+
+#..now merge test and trainig data
+#merge(mergedTestData, mergedTrainData, by.x=c("TestSubject","TestActivity"), by.y=c("TrainSubject","TrainActivity"))
+
+
+
