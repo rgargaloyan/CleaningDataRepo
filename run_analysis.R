30d625b51700fd96dd8f7d3f712b819660abb61d
rgargaloyan <rgargaloyan70@yahoo.com>
2014-10-27 13:42:27 -0700

    This is re-post of the proper file.
    Dear Reviewer, I re-submitted the code file on the next day (MON)
    however I did not make any change on the file created before the due
    date. I just initially submitted very old version from a different
    folder.

--- run_analysis.R
+++ run_analysis.R
@@ -7,7 +7,8 @@
 # From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 
-
+runAlalysys <- function() 
+{
 #.... First lets create our lookup vectors
 
 #... This will map the activity ID to the activity description
@@ -25,19 +26,62 @@ featureMap <-  as.list(setNames(featureFile$V1, featureFile$V2))
 
 #... We'll need to extract only required (mean and std) column indexes from this mess.
 #    Hence, the RegEx search pattern. 
-ptn <- "/(?:mean|std)/g"
+ptn <- "(?:mean|std)"
 validFeatureColumnIndexes <- subset(featureFile, grepl(ptn, featureFile$V2, perl=T))
 
-
+#### Test Data
 #...Get test data, eliminate unnecessary columns and combine it with Subject and Activity
-#mergedTestData <- mergeActivitySubjectData("Test","./test/subject_test.txt","./test/y_test.txt","./test/X_test.txt" )
+##...Read subjects (ids of people performing the test)
+dataTypePrefix <-  "Test"
+
+testSubjects <- read.csv("./test/subject_test_BIG.txt", header=FALSE)
+colnames(testSubjects) <- c(paste(dataTypePrefix,"Subjects",sep=""))
+
+#...Read Activities (type of activity, such as Walking, Seating, etc)
+testActivities <- read.csv("./test/y_test_BIG.txt",header=FALSE)
+colnames(testActivities) <- c(paste(dataTypePrefix,"Activities",sep=""))
+
+#...read the main data
+testData <- read.table("./test/X_test_BIG.txt", header=FALSE )
+#...eliminate irrelevant columns (leave only columns with names "mean" and "std")
+testData <- subset(testData,select=validFeatureColumnIndexes$V1)
+
+#... Set the readable column names
+colnames(testData) <- validFeatureColumnIndexes$V2
+
+#... Now we should merge features, subjects, actions and data together into a uniform data frame
+mergedTestDataSet <- data.frame(testSubjects,testActivities,testData)
+
+######### TRain Data
+
+dataTypePrefix <-  "Train"
+
+trainSubjects <- read.csv("./train/subject_train_BIG.txt", header=FALSE)
+colnames(trainSubjects) <- c(paste(dataTypePrefix,"Subjects",sep=""))
+
+#...Read Activities (type of activity, such as Walking, Seating, etc)
+trainActivities <- read.csv("./train/y_train_BIG.txt",header=FALSE)
+colnames(trainActivities) <- c(paste(dataTypePrefix,"Activities",sep=""))
+
+#...read the main data
+trainData <- read.table("./train/X_train_BIG.txt", header=FALSE )
+#...eliminate irrelevant columns (leave only columns with names "mean" and "std")
+trainData <- subset(trainData,select=validFeatureColumnIndexes$V1)
+
+#... Set the readable column names
+colnames(trainData) <- validFeatureColumnIndexes$V2
+
+#... Now we should merge features, subjects, actions and data together into a uniform data frame
+mergedTrainDataSet <- data.frame(trainSubjects,trainActivities,trainData)
 
 #...Get train data, eliminate unnecessary columns and combine it with Subject and Activity
-#mergedTrainData <- mergeActivitySubjectData("Train","./train/subject_train.txt","./train/y_train.txt","./train/X_train.txt" )
+#mergedTrainData <- mergeActivitySubject("Train","./train/subject_train.txt","./train/y_train.txt","./train/X_train.txt" )
 
 
 #..now merge test and trainig data
-#merge(mergedTestData, mergedTrainData, by.x=c("TestSubject","TestActivity"), by.y=c("TrainSubject","TrainActivity"))
-
-
-
+result <- merge(mergedTestDataSet, mergedTrainDataSet, by.x=c("TestActivities"), by.y=c("TrainActivities"))
+#apply
+aggdata <-aggregate(result, by=list(result$TestActivities, result$TestSubjects), FUN=mean, na.rm=TRUE)
+#tidySet <- tapply(orderedSubset$Hospital.Name, orderedSubset$State
+write.table(aggdata,"./aggregate.txt", sep=" ")
+}
